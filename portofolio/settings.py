04432650011de2a1d2cab2082a9bb9145a390dd4
Aloeasy <luisaloeasy@gmail.com>
2020-05-03 13:30:34 -0400

    JOBSAPP

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'job.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'portofoliodb',
+        'USER':'postgres',
+        'PASSWORD':'l1u2i3s4',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
